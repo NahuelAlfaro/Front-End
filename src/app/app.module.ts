cf28522eb6927a968b5db5109a44eef704a9b208
Nahuel Alfaro <nahuel_alfaro1@outlook.com>
2023-05-26 00:05:18 -0300

    creacioon desde 0 nuevo proyecto solucion angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { EduExpComponent } from './components/edu-exp/edu-exp.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutMeComponent,
+    EduExpComponent,
+    FooterComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    ProjectsComponent,
+    SkillsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/edu-exp/edu-exp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edu-exp',
+  templateUrl: './edu-exp.component.html',
+  styleUrls: ['./edu-exp.component.css']
+})
+export class EduExpComponent {
+
+}
